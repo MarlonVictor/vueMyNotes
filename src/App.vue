a7b62d0b0c1db8a499c0ad90375332306eac7a7b
MarlonVictor <cmarlonvictor11@gmail.com>
2024-02-06 22:54:53 -0300

    feat: adding favicon and logo to project with the first static element

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import logo from './assets/logo-nlw.svg'
 </script>
 
 <template>
-  <HelloWorld msg="Hello Word" />
+  <div class="max-w-6xl mx-auto my-12 space-y-6">
+    <img :src="logo" alt="NLW Expert" />
+
+    <form>
+      <input 
+        type="text" 
+        placeholder="Busque em suas notas..." 
+        class="bg-transparent w-full text-3xl font-semibold tracking-tight outline-none placeholder:text-slate-500"
+      />
+    </form>
+
+    <div class="h-px bg-slate-700" />
+  </div>
 </template>
 
 <style scoped>
